8bf6f65f55cf5ffe72a67b603d8d9b119c6af940
TechnoStasis <yvesmiguelreyes1@gmail.com>
2024-05-02 14:53:50 +0800

    Better formalize automated Trello card generation

--- create_card.py
+++ create_card.py
@@ -9,33 +9,57 @@ load_dotenv()
 auth_token = os.getenv("AUTH_TOKEN")
 api_key = os.getenv("API_KEY")
 
-list = "6632e70f9c08b15bc441c7cf"
-
-url = "https://api.trello.com/1/cards"
-
 headers = {
   "Accept": "application/json"
 }
 
 query = {
-  'idList': list,
   'key': api_key,
   'token': auth_token,
 }
 
 
-def create_card(list, name):
-  query["name"] = name
+def create_card(list, name, desc):
+  queryCopy = dict(query)
+  queryCopy["name"] = name
+  queryCopy["idList"] = list
+  queryCopy["desc"] = desc
+
+  url = "https://api.trello.com/1/cards"
 
   response = requests.request(
     "POST",
     url,
     headers=headers,
-    params=query
+    params=queryCopy
   )
-  # JSON_Response = json.loads(response.text)
-  # print(json.dumps(JSON_Response, sort_keys=True, indent=4, separators=(",", ": ")))
+  JSON_Response = json.loads(response.text)
+  
+  return JSON_Response
+
+def create_list(id, name, templateId):
+  url = "https://api.trello.com/1/cards/" + id + "/checklists"
+
+  queryCopy = dict(query)
+  queryCopy["name"] = name
+  queryCopy["idChecklistSource"] = templateId
+
+  response = requests.request(
+   "POST",
+   url,
+   params=queryCopy
+  )
+
+  return json.loads(response.text)
+
+  print(response.text)
 
 
+
+
+##Testing
 if __name__ == "__main__":
-    create_card(list, 'WOAH!!')
+    BUILD_CHECKLIST = os.getenv("BUILD_CHECKLIST")
+    TEST_LIST = os.getenv("TEST_LIST")
+    create_list(create_card(TEST_LIST, 'Testing', "Test!")["id"], "Test Checklist", BUILD_CHECKLIST)
+    
