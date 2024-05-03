be2c42f1831e4ab43102b8d51f689696c1da38b1
TechnoStasis <yvesmiguelreyes1@gmail.com>
2024-05-03 10:52:06 +0800

    Basic excel parser

---
+++ excel_parser.py
@@ -0,0 +1,20 @@
+import openpyxl
+
+def parse_spreadsheet_for_pickslips_and_due_dates(file):
+    data = []
+
+    dataframe = openpyxl.load_workbook(file)
+    spreadsheet = dataframe.active
+
+    for col in spreadsheet.iter_cols(1, spreadsheet.max_column):
+        column = []
+        if col[0].value == "Pickslip" or col[0].value == "Creation date":
+            for row in range(1, spreadsheet.max_row):
+                    column.append(col[row].value)
+            data.append(column)
+
+
+    return data
+
+
+print(parse_spreadsheet_for_pickslips_and_due_dates("Test Excel Sheet.xlsx"))
