0cb7dea9ee5816b1bb6cd93c61a03afde5605228
TechnoStasis <yvesmiguelreyes1@gmail.com>
2024-05-21 09:35:08 +0800

    woop

--- spreadsheet_reporter.py
+++ spreadsheet_reporter.py
@@ -2,4 +2,5 @@ from variables import *
 import trello_imports
 import datetime
 
-trello_imports.create_excel_file(FIT_OUT_BOARD,  "/reports/" +  str(datetime.datetime.now()).replace(':','.') +  " fit out report.xlsx")
+if __name__ == "__main__":
+    trello_imports.create_excel_file(FIT_OUT_BOARD,  "/reports/" +  str(datetime.datetime.now()).replace(':','.') +  " fit out report.xlsx")
