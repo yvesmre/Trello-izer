12e6a5ae193765a320b4a1a70f15cb66232cdce4
Yves Miguel <7437383+yvesmre@users.noreply.github.com>
2024-06-18 14:41:45 +0800

    Change to excel file on build out sheet

--- trello_print_out.py
+++ trello_print_out.py
@@ -2,6 +2,8 @@ import tkinter
 from functools import partial
 from trello_imports import *
 from docx import Document
+from docx.shared import Inches
+import os
 
 def fit_text_to_widget(text_widget):
     # Get the number of lines and the longest line's length
@@ -24,12 +26,70 @@ def all_children (wid) :
 
     return _list
 
+
+
+
+def create_spreadsheet(board_id, job_no, filename):
+
+    #Hacky way of going about this, tbh
+    if not os.path.exists(os.getcwd() + "/" + filename.split('/')[1]) :
+        os.makedirs(os.getcwd() + "/" + filename.split('/')[1])
+
+    board = import_cards_with_custom_fields_from_board(board_id)
+
+    to_json = {"Checklist": [], 
+                'Part No.': [], 
+            #    "Member": [], "Completion": [], 
+            #    "Card Last Modified": []
+                    }
+
+    user_to_name = {}
+    for card in board:
+        checklists = import_checklist(card['id'])
+
+        card_name = card['name']
+        if not (card_name.split('-')[0].replace('#', '').strip() == job_no): continue
+
+        for obj in checklists:
+
+            part_found = None
+            for checklist_item in obj["checkItems"]:
+                if("Part No." in checklist_item['name']):
+                    part_found = checklist_item['name']
+                    break
+            
+    
+            to_json['Checklist'].append(obj['name'])
+            to_json['Part No.'].append('N/A' if not part_found else part_found)
+
+        break
+
+    frame = pd.DataFrame(data=to_json)
+
+
+    writer = StyleFrame.ExcelWriter(os.getcwd() + filename)
+    sf = StyleFrame(frame)
+
+    #   sf.set_column_width(columns='Card Title', width=25)
+    sf.set_column_width(columns='Checklist', width=70)
+    sf.set_column_width(columns='Part No.', width=50)
+    #   sf.set_column_width(columns='Member', width=25)
+    #   sf.set_column_width(columns='Card Last Modified', width=20)
+    #   sf.set_column_width(columns='Completion', width=20)
+    sf.to_excel(excel_writer=writer, row_to_add_filters=0,index=False)
+
+
+    writer.close()
+
+
+
+
 if __name__ == "__main__":
     def search_card(widget):
         board = import_cards_with_custom_fields_from_board(FIT_OUT_BOARD if not USE_TESTING_LIST else TEST_LIST)
 
 
-
+        create_spreadsheet(board_id=FIT_OUT_BOARD, job_no=widget.get(), filename="/output/" + widget.get() + ".xlsx")
         for child in all_children(m):
             if type(child) == tkinter.Text:
                 child.destroy()
@@ -71,3 +131,4 @@ if __name__ == "__main__":
     start_button.grid(row=2, column=1)
 
     m.mainloop()
+
